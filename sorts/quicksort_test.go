33b9cc256273fb83ce56c74c422e69a07c47e3c3
Robert Rizzo <robtrizzo@gmail.com>
2025-02-12 00:00:48 -0500

    quicksort

---
+++ sorts/quicksort_test.go
@@ -0,0 +1,13 @@
+package sorts
+
+import (
+	"robtrizzo/data-and-algo-practice/internal/assert"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	expected := []int{1, 2, 3, 4, 5}
+	actual := []int{2, 1, 4, 5, 3}
+	QuickSort(&actual)
+	assert.ArraysEqual[int](t, actual, expected)
+}
