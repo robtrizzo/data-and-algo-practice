695c7790f1a4fea6bd4206f16ef2fe8aa8e45b61
Robert Rizzo <robtrizzo@gmail.com>
2025-02-17 11:22:13 -0500

    trie suggestions

--- trees/trie.go
+++ trees/trie.go
@@ -33,7 +33,32 @@ func (t *Trie) Search(word string) bool {
 	return n.isWord
 }
 
-// TODO
 func (t *Trie) Suggestions(substring string, depth int) []string {
-	return []string{}
+	n := t.root
+	for _, r := range substring {
+		_, exists := n.children[r]
+		if !exists {
+			return []string{}
+		}
+		n = n.children[r]
+	}
+
+	return t.collectWords(n, substring, len(substring)+depth)
+}
+
+func (t *Trie) collectWords(n *TrieNode, substring string, maxLen int) []string {
+	var words []string
+	if n.isWord {
+		words = append(words, substring)
+	}
+
+	if len(substring) == maxLen {
+		return words
+	}
+
+	for r, child := range n.children {
+		newSubstring := substring + string(r)
+		words = append(words, t.collectWords(child, newSubstring, maxLen)...)
+	}
+	return words
 }
